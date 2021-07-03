4c8b62e801d34010236675e0ff808f13d90a0ca0
Adriana Scrobote <adriana_100000@hotmail.com>
2021-07-03 13:47:16 -0300

    feat(database): adicionando variáveis de ambiente

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const express = require('express')
+require('dotenv').config()
 
 const app = express()
 
--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,11 @@
 const pgp = require('pg-promise')()
 
 const db = pgp({
-  user: 'postgres',
-  password: 'admin',
-  host: 'localhost',
-  port: 5432,
-  database: 'midway'
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
 })
 
 module.exports = db
